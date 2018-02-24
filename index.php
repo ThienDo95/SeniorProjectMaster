ea91eef651ba214abe23df9f43e45b8ad15c4da6
Samuel Liebe-Randall <lieberandall@gmail.com>
2018-02-24 16:49:52 -0500

    New Homepage

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+include ('includes.php');
+
+include ('header.php');
+?>
+
+            <div class="content">
+                <div class="title m-b-md">
+                    PI Face Validation
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Project Spec</a>
+					<a href="https://docs.google.com/document/d/1VC-30A1skUeAFvtEB7_fPx2EonJ1Y7I5OZxMB6WO3z0/edit?usp=sharing">Design Doc</a>  					
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+            </div>
+       
+    </body>
+</html>
