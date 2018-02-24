69be0db56662cc74fafa0cc8a7e076030938ba15
Thien Do <sora4everblue@gmail.com>
2018-02-24 16:33:50 -0500

    thien pushing updated profile page

--- profile.php
+++ profile.php
@@ -9,18 +9,46 @@ include ('header.php');
  <center> <h3>Welcome User!</h3> </center>
  <center> <h4>To begin, please click on <b> Add User </b> to add a Rasberry Pi to your account </h4> </center>
 
-<center><a href="addDevice" class="btn btn-default">Add User</a></center>
+<center>
+<button type="button" class="btn btn-primary">
+Add Device
+</button>
+<button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
+Upload Picture
+</button>
+</center>
 
-<body>
-    <div class ="container">
-      <div class ="content">
-        <center> <h1> Upload Your Picture </h1> </center>
-        <form action ="" method="Post" enctype ="multipart/form-data">
-          <center><h4> Select image to upload: </h4></center>
-          <center><input type ="File" name"file" id= "file"><input type ="submit" value ="Upload" name="submit"></center>
-        </form>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          X
+        </button>
+      </div>
+      <div class="modal-body">
+
+		  <div class ="content">
+			<form action ="" method="Post" enctype ="multipart/form-data">
+			  <center><h4> Select image to upload: </h4></center>
+			  <center><input type ="File" name"file" id= "file"><input type ="submit" value ="Upload" name="submit"></center>
+			</form>
+		  </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </div>
     </div>
-</body>
+  </div>
+</div>
 
+
+
+
+
+</body>
 </html>
