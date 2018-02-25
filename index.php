3295f058683f8795e6aeac229fa49e50c0f4e6f9
Joseph Dementri <joedementri42012@gmail.com>
2018-02-25 11:55:12 -0500

    Added fancy gif and links to spec, doc, and github

--- index.php
+++ index.php
@@ -3,19 +3,21 @@
 include ('includes.php');
 
 include ('header.php');
+
 ?>
 
             <div class="content">
                 <div class="title m-b-md">
                     PI Face Validation
+					<img src="https://cdn-images-1.medium.com/max/1600/1*N5v3U_mIz3dF0skheNd4VQ.gif">
                 </div>
-
+				
                 <div class="links">
-                    <a href="https://laravel.com/docs">Project Spec</a>
+                    <a href="https://docs.google.com/document/d/1qOy5YBhhL0QJwJJAKamMzgiyNWKW2BG8yjezxC5MtuQ/edit?usp=sharing">Project Spec</a>
 					<a href="https://docs.google.com/document/d/1VC-30A1skUeAFvtEB7_fPx2EonJ1Y7I5OZxMB6WO3z0/edit?usp=sharing">Design Doc</a>  					
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/ThienDo95/SeniorProjectMaster">GitHub</a>
                 </div>
             </div>
-       
-    </body>
-</html>
+      
+	 
+<?php include ('footer.php');?>
