328bb57494970087722c118b9d50eee4cd3e9f40
Thien Hoang Tran Do <thiendo411@gmail.com>
2018-03-28 21:10:12 -0400

    Add files via upload
    Properly sent email and formatted

--- forgot-password.php
+++ forgot-password.php
@@ -1,8 +1,8 @@
 <?php
 
 include ('includes.php');
-
 include ('header.php');
+include ('send_link.php');
 
 ?>
 
@@ -13,54 +13,21 @@ include ('header.php');
                 <div class="panel-heading">Reset Password</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="forgot-password.php">        
+                    <form class="form-horizontal" method="POST" action="send_link.php">        
 
                         <input type="hidden" name="token" value="">
 
-                        <div>
+                        <div class="form-group">
                             <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="" required autofocus>
-
-                                <!--
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                -->
-                            </div>
-                        </div>
-
-                        <div>
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                <!--
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                              -->
-                            </div>
-                        </div>
-
-                        <div>
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-
-                                <!--
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                                -->
                             </div>
                         </div>
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" name="submit_email">
                                     Reset Password
                                 </button>
                             </div>
@@ -72,5 +39,4 @@ include ('header.php');
     </div>
 </div>
 
-</body>
-</html>
+<?php include ('footer.php');?>
